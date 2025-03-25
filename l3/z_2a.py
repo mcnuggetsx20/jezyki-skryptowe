689d3e1f42cbaa08bbd0118ccc6f4650e2607103
kla <kacper.klasen@gmail.com>
2025-03-25 14:30:32 +0100

    commit

--- l3/z_2a.py
+++ l3/z_2a.py
@@ -5,14 +5,15 @@ def read_log():
     logs = []
     for line in sys.stdin:
         try:
-            kr = line.split('\t')[:10]
+            
+            kr = line.split('\t')
+            kr = kr[:10] + [kr[14]]
             kr[3] = int (kr[3])
             kr[5] = int(kr[5])
             kr[6] = int(kr[6])
             kr[0] = datetime.datetime(1970, 1, 1) + datetime.timedelta(seconds=float(kr[0]))
-            
             logs.append(kr)
-        except:
+        except Exception as e:
             continue
     return logs
 
