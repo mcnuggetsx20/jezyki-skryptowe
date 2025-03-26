f8797cd31182433e9f3be4b3105f887b89b35b8f
mcnuggetsx20 <alienbaxy@gmail.com>
2025-03-26 11:20:11 +0100

    26 Mar 2025 11:20:11

--- l3/dicts.py
+++ l3/dicts.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def entry_to_dict(entry):
     keys = [
         'ts',
@@ -14,7 +16,6 @@ def entry_to_dict(entry):
     return dict(zip(keys, entry))
 
 def log_to_dict(log):
-    from collections import defaultdict
     result = defaultdict(lambda: list())
 
     for entry in log:
@@ -26,20 +27,32 @@ def log_to_dict(log):
 def print_dict_entry_dates(dict_log):
     from lib import sort_log
 
+    method_dict = defaultdict(lambda: 0)
+    host_dict = defaultdict(lambda: 0)
+
     for k, v in dict_log.items():
         print(k)
 
         num_queries = 0
-        tuple_log = list()
         for dict_entry in v:
             print(dict_entry['host'], dict_entry['uri'])
-            tuple_log.append(tuple(dict_entry.values()))
+            method_dict[dict_entry['method']] += 1
+            host_dict[dict_entry['host']] += 1
 
             num_queries += 1
 
-        sorted_by_date = sort_log(tuple_log, 0)
+        sorted_by_date = sort_log(v, 'ts')
         print(f'number of queries: {num_queries}')
-        print(f'first: {sorted_by_date[0][0]} \nlast: {sorted_by_date[-1][0]}')
+        print(f'first: {sorted_by_date[0]['ts']} \nlast: {sorted_by_date[-1]['ts']}')
         print()
+
+    method_sum = sum(method_dict.values())
+    for k, v in method_dict.items():
+        print(f'{k}: {round(v/method_sum * 100, 5)}%')
+
+    print("\nHOSTS:")
+    for k, v in host_dict.items():
+        print(f'{k}: {v}')
+
 
 
