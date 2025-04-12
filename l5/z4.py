3596861e2efc1e700bbfaeb1e66ddad99e00f51c
mcnuggetsx20 <alienbaxy@gmail.com>
2025-04-12 09:28:15 +0200

    12 Apr 2025 09:28:15

--- l5/z4.py
+++ l5/z4.py
@@ -1,4 +1,3 @@
-import csv
 import re
 
 dates = []
@@ -9,28 +8,36 @@ immobile = []
 three_piece = []
 addr_with_comma = []
 
-with open('stacje.csv' ,'r', encoding='utf-8') as file:
-    headers = [i for i in csv.DictReader(file).fieldnames]
+comma_pattern = r',(?=(?:[^"]*"[^"]*")*[^"]*$)'
+split_pattern = r'\n(?=(?:[^"]*"[^"]*")*[^"]*$)'
 
-    for line in file:
-        dates.extend(re.findall(r'\d\d\d\d-\d\d-\d\d', line))
+file = open('stacje.csv', 'r')
+filedata = file.read().strip()
+filedata = re.split(split_pattern, filedata)
+file.close()
 
-        lat_lon.extend(re.findall(r'\d{2}\.\d{6}', line))
+headers = [i for i in re.split(comma_pattern, filedata[0].strip())]
 
-        stations.extend(re.findall(r'\b\w+\b\s*-\s*\b\w+\b', line))
+for line in filedata[1:]:
 
-        # WOW
-        values = re.split(r',(?=(?:[^"]*"[^"]*")*[^"]*$)', line.strip()) 
-        dt = dict(zip(headers, values))
-        if re.findall(r'.*MOB', dt['Kod stacji']):
-            if dt['Rodzaj stacji'].lower() != 'mobilna':
-                immobile.append(dt)
+    dates.extend(re.findall(r'\d\d\d\d-\d\d-\d\d', line))
 
-        if re.findall(r'\b\w+\b\s*-\s*\b\w+\b\s*-\s*\b\w+\b', dt['Nazwa stacji']):
-            three_piece.append(dt['Nazwa stacji'])
+    lat_lon.extend(re.findall(r'\d{2}\.\d{6}', line))
 
-        if re.findall(r'^.*(?:ul\.|al\.).*,|,.*(?:ul\.|al\.).*$', dt['Nazwa stacji']):
-            addr_with_comma.append(dt['Nazwa stacji'])
+    stations.extend(re.findall(r'\b\w+\b\s*-\s*\b\w+\b', line))
+
+    # WOW
+    values = re.split(comma_pattern, line.strip()) 
+    dt = dict(zip(headers, values))
+    if re.findall(r'.*MOB', dt['Kod stacji']):
+        if dt['Rodzaj stacji'].lower() != 'mobilna':
+            immobile.append(dt)
+
+    if re.findall(r'\b\w+\b\s*-\s*\b\w+\b\s*-\s*\b\w+\b', dt['Nazwa stacji']):
+        three_piece.append(dt['Nazwa stacji'])
+
+    if re.findall(r'^.*(?:ul\.|al\.).*,|,.*(?:ul\.|al\.).*$', dt['Nazwa stacji']):
+        addr_with_comma.append(dt['Nazwa stacji'])
 
 pl_dict = {
     'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l', 'ń': 'n', 'ó': 'o', 'ś': 's', 'ź': 'z', 'ż': 'z',
