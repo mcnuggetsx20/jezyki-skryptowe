c3219bfa187d9652116965d60c59dbc6ea0b77aa
mcnuggetsx20 <alienbaxy@gmail.com>
2025-04-15 14:38:41 +0200

    15 Apr 2025 14:38:40

---
+++ l6/z1.py
@@ -0,0 +1,57 @@
+#Nr,Kod stacji,Kod międzynarodowy,Nazwa stacji,"Stary Kod stacji 
+#(o ile inny od aktualnego)",Data uruchomienia,Data zamknięcia,Typ stacji,Typ obszaru,Rodzaj stacji,Województwo,Miejscowość,Adres,WGS84 φ N,WGS84 λ E
+import lib
+class Station:
+    def __init__(
+            self,
+            nr,
+            kod_stacji,
+            kod_miedz,
+            nazwa,
+            stary_kod,
+            data_begin,
+            data_end,
+            typ,
+            obszar,
+            rodzaj,
+            woj,
+            miejsc,
+            adres,
+            lon,
+            lat
+    ):
+        for name, value in locals().items():
+            if name != 'self':
+                setattr(self, name, value)
+
+        return
+
+    def __str__(self):
+        res = ''
+        for k, v in self.__dict__.items():
+            res += f'{k}: {v};'
+        return res
+    
+    def __repr__(self):
+        cls_name = self.__class__.__name__
+        attrs = ', '.join(f"{k}={repr(v)}" for k, v in self.__dict__.items())
+        return f"{cls_name}({attrs})"
+    
+    def __eq__(self, other):
+        return self.kod_stacji == other.kod_stacji
+
+if __name__ == '__main__':
+    filedata = lib.read_stations()
+    args1 = lib.split_line(filedata[3])
+    args2 = lib.split_line(filedata[4])
+
+    st1 = Station(*args1)
+    st2 = Station(*args2)
+
+    st2.kod_stacji = st1.kod_stacji
+    print(st1 == st2)
+
+
+
+
+
