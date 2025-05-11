88baea931ec7a995585c9a4bdfc5b90d65620802
mcnuggetsx20 <alienbaxy@gmail.com>
2025-05-11 23:55:53 +0200

    11 May 2025 23:55:53

--- l8/main.py
+++ l8/main.py
@@ -22,10 +22,6 @@ class App:
         self.current_linenr = None
         self.detail_vars = []
 
-        self.root.grid_columnconfigure(0, weight = 1)
-        self.root.grid_columnconfigure(1, weight = 1)
-        self.root.grid_columnconfigure(2, weight = 1)
-
         ## HEADER
         self.header_frame = tkinter.Frame(self.root)
         self.header_frame.grid_columnconfigure(1, weight = 1)
@@ -39,13 +35,33 @@ class App:
         tkinter.Button(self.header_frame, text="Zamknij", command=self.root.destroy).grid()
         ##
 
-        ## DETAIL
-        self.detail_frame = tkinter.Frame(self.root)
-        self.detail_frame.pack(side='top', fill='x')
+        ## MAIN
+        self.main_frame = tkinter.Frame(self.root)
+        self.main_frame.pack(fill='both', expand=True, padx=10)
 
-        self.text_field = tkinter.Text(self.detail_frame, wrap='none')
-        self.text_field.grid(row=0, column=0)
+        self.main_frame.grid_columnconfigure(0, weight=0)  # DETAIL - nie rośnie
+        self.main_frame.grid_columnconfigure(1, weight=1)  # LOG - główny
+        self.main_frame.grid_columnconfigure(2, weight=0)  # NAV - wąski
+
+        ## DETAIL
+        self.detail_frame = tkinter.Frame(self.main_frame)
+        self.detail_frame.grid(row=0, column=0, sticky='n') 
+        ##
+
+        ## LOG
+        self.log_frame = tkinter.Frame(self.main_frame)
+        self.log_frame.grid(row=0, column=1, sticky='nsew')
+        self.text_field = tkinter.Text(self.log_frame, wrap='none',width=50)
+        # self.text_field.grid(row=0, column=0)
         self.text_field.bind("<Key>", lambda _: 'break')
+        self.text_field.pack()
+        ##
+
+        ## NAVIGATION
+        self.nav_frame = tkinter.Frame(self.main_frame)
+        self.nav_frame.grid(row=0, column=2, sticky='ns')
+        ##
+
 
         return
 
@@ -57,11 +73,16 @@ class App:
         if path:
             print(f"Chose file: {path}")
 
+            for i in self.detail_frame.winfo_children():
+                i.destroy()
+
+            self.current_linenr = 0
             self.file_path.text.set(path)
             self.file_data = lib.read_log_from_path(path)
             self.display_log()
             self.prepare_details()
-            self.display_details(0)
+            self.display_details(self.current_linenr)
+            self.highlight_line(self.current_linenr+1)
 
         return
 
@@ -81,7 +102,6 @@ class App:
 
         self.clear_text_field()
         self.update_text_field(strings)
-        # self.highlight_line(2)
 
         return
 
@@ -94,12 +114,15 @@ class App:
             self.text_field.insert(tkinter.END, line + '\n')
         return
 
-    def highlight_line(self, line_number):
+    def highlight_line(self, line_number, reset=False):
         start = f"{line_number}.0"
         end = f"{line_number+1}.0"
 
-        self.text_field.tag_add(f"line{line_number}", start, end)
-        self.text_field.tag_configure(f"line{line_number}", background="yellow")
+        if reset:
+            self.text_field.tag_remove("highlight", "1.0", "end")
+
+        self.text_field.tag_add("highlight", start, end)
+        self.text_field.tag_configure("highlight", background="yellow")
 
     def prepare_details(self, log = None):
         if log is None:
@@ -112,8 +135,12 @@ class App:
 
         self.detail_vars = [self.InnerElement(i, '', self.detail_frame) for i in dicts.get_keys()]
         for i, _ in enumerate(self.detail_vars):
-            self.detail_vars[i].title_label.grid(row=i, column=1)
-            self.detail_vars[i].label.grid(row=i, column=2)
+            self.detail_vars[i].title_label.grid(row=i, column=0,sticky='w')
+            self.detail_vars[i].label.grid(row=i, column=1, sticky='w')
+
+        tkinter.Button(self.nav_frame, text='<-', command=lambda: self.next_log(dir=-1)).grid(row=len(self.detail_vars)-1, column = 5)
+        tkinter.Button(self.nav_frame, text='->', command=lambda: self.next_log(dir=1)).grid(row=len(self.detail_vars)-1, column = 6)
+
         return
 
     def display_details(self, linenr = None):
@@ -121,13 +148,24 @@ class App:
             if self.current_linenr is None:
                 return
             linenr = self.current_linenr
-            
+        if not self.file_data: return
 
         dct = dicts.entry_to_dict(self.file_data[linenr])
         for _, ((_, value), var) in enumerate(zip(dct.items(), self.detail_vars)):
             var.text.set(value)
         return
         
+    def next_log(self, dir=0):
+        if not dir: return
+        if self.current_linenr is None:
+            self.current_linenr = 0
+
+        dir = min(1, max(-1, dir))
+
+        self.current_linenr += dir
+        self.display_details(self.current_linenr)
+        self.highlight_line(self.current_linenr+1, reset=True)
+        return
 
     def run(self):
         self.root.mainloop()
