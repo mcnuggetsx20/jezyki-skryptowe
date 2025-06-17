921bcb073fd852f5f5932087ebcdb733daed59f9
kla <kacper.klasen@gmail.com>
2025-06-17 11:00:23 +0200

    controlling camera

--- projekt/GUI/camera_control_frame.py
+++ projekt/GUI/camera_control_frame.py
@@ -6,6 +6,10 @@ import threading
 import io
 import time
 import cv2
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from lib.commands import *
 
 class CameraControlFrame(ttk.Frame):
     def __init__(self, container, dev, app, height=480, width=640):
@@ -23,11 +27,15 @@ class CameraControlFrame(ttk.Frame):
 
         # Opcje z boku
         self.motion_var = tk.BooleanVar()
-        self.motion_chk = ttk.Checkbutton(self.sidebar, text="Wykrywanie ruchu", variable=self.motion_var)
+        self.motion_chk = ttk.Checkbutton(self.sidebar, text="Wykrywanie ruchu", variable=self.motion_var, command=self.toggle_move_detection)
         self.motion_chk.pack(anchor="w", pady=5)
 
-        self.status = ttk.Label(self.sidebar, text="Status: brak połączenia")
+        self.status = ttk.Label(self.sidebar, text="Nie wykryto ruchu")
         self.status.pack(anchor="w", pady=5)
+
+        self.camera_stream = tk.BooleanVar()
+        self.camera_stream_chk = ttk.Checkbutton(self.sidebar, text="Camera Stream", variable=self.camera_stream, command=self.toggle_camera_stream_detection)
+        self.camera_stream_chk.pack(anchor="w", pady=5)
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
@@ -41,30 +49,35 @@ class CameraControlFrame(ttk.Frame):
         thread.start()
         self.tk_image = None
         self.after(50, self.update_now)
+        self.timing = time.time()
+        self.after(100, self.update_move)
 
     def receive_commands_from_server(self):
-        while self.running:
-            # Iteruj po wszystkich frame'ach w kolejce serwera
-            while self.app.server.recv_queue:
-                frame_info = self.app.server.recv_queue.pop(0) 
+        try:
+            while self.running:
+                # Iteruj po wszystkich frame'ach w kolejce serwera
+                while self.app.server.recv_queue:
+                    frame_info = self.app.server.recv_queue.pop(0) 
 
-                # Sprawdź, czy ten frame jest dla device
-                fd = frame_info['fd']
+                    # Sprawdź, czy ten frame jest dla device
+                    fd = frame_info['fd']
 
-                if fd != self.dev['fd']:
-                    print(fd)
-                    print(self.dev['fd'])
-                    continue  # pomiń, jeśli nie dotyczy
-                if(frame_info['command'] == 3):
-                    data = frame_info['data']
-                    if data is None:
-                        break
-                    rgb_frame = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
-                    pil_image = Image.fromarray(rgb_frame)
+                    if fd != self.dev['fd']:
+                        print(fd)
+                        print(self.dev['fd'])
+                        continue  # pomiń, jeśli nie dotyczy
+                    if(frame_info['command'] == COMMAND_CAMERA_STREAM):
+                        data = frame_info['data']
+                        if data is None:
+                            break
+                        rgb_frame = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
+                        pil_image = Image.fromarray(rgb_frame)
 
-                    self.update_image(pil_image)
- 
-            time.sleep(0.06)
+                        self.update_image(pil_image)
+                    elif (frame_info['command'] == COMMAND_CAMERA_MOVE_DETECTED):
+                        self.timing = time.time()
+                time.sleep(0.06)
+        except: pass
 
     def update_image(self, pil_image):
         img_w, img_h = pil_image.size
@@ -102,4 +115,27 @@ class CameraControlFrame(ttk.Frame):
     def destroy(self):
         self.running = False
         self.app.clear_buffer = True
+        self.motion_var.set(False)
+        self.toggle_move_detection()
+        self.camera_stream.set(False)
+        self.toggle_camera_stream_detection()
         super().destroy()
+    
+    def toggle_move_detection(self):
+        value = 1 if self.motion_var.get() else 0
+        msg = bytes([COMMAND_CAMERA_MOVE_DETECTED_OFF_ONN, value])
+        self.app.server.send_to_client(self.dev['ip'], msg)
+    
+    def toggle_camera_stream_detection(self):
+        value = 1 if self.camera_stream.get() else 0
+        msg = bytes([COMMAND_CAMERA_STREAM_OFF_ONN, value])
+        self.app.server.send_to_client(self.dev['ip'], msg)
+    
+    def update_move(self):
+        if time.time() - self.timing < 1:  # 2 sekundy
+            self.status.config(text="Ruch wykryty!!!!!")
+        else:
+            self.status.config(text="Nie wykryto ruchu")
+
+        self.after(100, self.update_move)
+        
--- projekt/camera.py
+++ projekt/camera.py
@@ -5,19 +5,20 @@ import time
 from lib.types import *
 from lib.commands import *
 
-STREAMING = True
-MOVE_DETECTION = True
+
 
 class CameraCMDHandler:
-    def __init__():
+    def __init__(self):
+        self.streming = False
+        self.move_detection= False
         return
 
     def handle_data(self, msg):
         if COMMAND_CAMERA_STREAM_OFF_ONN == msg[0]:
-            STREAMING = bool(msg[1])
+            self.streming = msg[1]
         
         elif COMMAND_CAMERA_MOVE_DETECTED_OFF_ONN == msg[0]:
-            MOVE_DETECTION = bool(msg[1])
+            self.move_detection = msg[1]
 
 
 
@@ -62,8 +63,10 @@ def detect_motion(prev_frame: cv2.Mat, curr_frame: cv2.Mat, min_area=500) -> boo
     return False
 
 if __name__ == '__main__':
+    counter = 0
     camera, width, height = getCam()
-    cl = client.Client(tp=TYPE_CAMERA, identity='kamerka')
+    camera_handler = CameraCMDHandler()
+    cl = client.Client(tp=TYPE_CAMERA, identity='kamerka', input_handler=camera_handler)
     cl.prepare()
 
     ret, prev_frame = camera.read()
@@ -73,21 +76,22 @@ if __name__ == '__main__':
             if not ret:
                 raise Exception("Failed to read frame!")
 
-            if MOVE_DETECTION and detect_motion(prev_frame, frame):
+            if camera_handler.move_detection and detect_motion(prev_frame, frame) and counter%5 == 0:
                 print('byl ruch!')
+                counter = counter%5
                 if cl.client_connected:
                     cl.add_to_send(struct.pack('!B', COMMAND_CAMERA_MOVE_DETECTED))
 
             prev_frame = frame
-            if cl.client_connected and STREAMING:
-                _, encoded = cv2.imencode('.jpg', frame)
+            if cl.client_connected and camera_handler.streming:
+                _, encoded = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
                 data = encoded.tobytes()
                 cl.add_to_send(struct.pack('!BI', 
                     COMMAND_CAMERA_STREAM, len(data)) + data)
 
 
             cl.pollEvents(timeout=0)
-            time.sleep(0.05)
+            time.sleep(0.02)
     except KeyboardInterrupt:
         camera.release()
         cv2.destroyAllWindows()
